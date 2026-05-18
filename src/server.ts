5a1ff8fe219f996705982a63b9fe23b1bc5e7cff
Rifat Hasan <rifathasannetrakona@gmail.com>
2026-05-18 15:57:12 +0600

    added post route

--- src/server.ts
+++ src/server.ts
@@ -6,24 +6,71 @@ const server: Server = http.createServer((req: IncomingMessage, res: ServerRespo
 
     console.log("server is running ...");
 
-    if (req.url == "/" && req.method=="GET") {
-        res.writeHead(200, {"content-type": "application/json"});
+    // root route
+    if (req.url == "/" && req.method == "GET") {
+        res.writeHead(200, { "content-type": "application/json" });
         res.end(JSON.stringify({
             message: "success",
-            path:req.url,
+            path: req.url,
             data: {
                 name: "Rifat",
                 age: 25
             }
         }));
-        
+
     }
-    
+
+    // health route
+    if (req.url == "/health" && req.method == "GET") {
+        res.writeHead(200, { "content-type": "application/json" });
+        res.end(JSON.stringify({
+            message: "health status okey",
+            path: req.url,
+        }));
+    }
+
+    // post route
+    if (req.url == "/api/users" && req.method == "POST") {
+
+        //     const user = {
+        //         id: 1,
+        //         name: "Rifat",
+        //         email: "[EMAIL_ADDRESS]"
+        //     }
+
+        //     res.writeHead(200, { "content-type": "application/json" });
+        //     res.end(JSON.stringify(user));
+
+
+        let body = "";
+
+        req.on("data", (chunk) => {
+            body += chunk;
+        })
+
+        req.on("end", () => {
+            try {
+                const parseBody = JSON.parse(body);
+                console.log(parseBody)
+                res.end(JSON.stringify(parseBody));
+
+            } catch (error: any) {
+                console.log(error.message)
+                res.writeHead(400, { "content-type": "application/json" });
+                res.end(JSON.stringify({
+                    message: "Invalid JSON"
+                }));
+            }
+        })
+
+
+
+    }
 });
 
 
 
 
-server.listen(config.port, ()=>{
+server.listen(config.port, () => {
     console.log(`server is running on port ${config.port} in ${config.env} mode`);
 })
