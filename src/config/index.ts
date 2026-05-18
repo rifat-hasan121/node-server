457d7ec5bd72647cc464c1c7d5018d6f5b27eda0
Rifat Hasan <rifathasannetrakona@gmail.com>
2026-05-18 14:57:28 +0600

    sucessfuly added a node.js server with properly scrict env variable

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,3 +4,12 @@ import path from "path";
 dotenv.config({
     path: path.join(process.cwd(), ".env"),
 });
+
+
+const config = {
+    env: process.env.NODE_ENV ? Number(process.env.NODE_ENV) : 5000,
+    port: process.env.PORT ? Number(process.env.PORT) : 5000,
+    
+}
+
+export default config;
